aed9f7c0e181898ab82bba1ca60271e9ad49c6b4
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-04-16 00:54:24 -0500

    Asdasda

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -2,18 +2,35 @@ from app.models import db, Product
 
 
 def seed_products():
-    first = Product(user_id=1,name='Perfect',description='A',image_url='https://images.ctfassets.net/u0haasspfa6q/1uGsqwmSpOUN3ITzZ0akhp/7582e3072d2da76fdc3689527f808268/girl-in-the-sunset-on-some-rocks-on-the-water'
-    ,price=1,category='Food',quantity=2)
-    second = Product(user_id=2,name='Good',description='S',image_url='https://images.ctfassets.net/u0haasspfa6q/1uGsqwmSpOUN3ITzZ0akhp/7582e3072d2da76fdc3689527f808268/girl-in-the-sunset-on-some-rocks-on-the-water'
-    ,price=2,category='Food',quantity=2)
-    third= Product(user_id=3,name='Meh',description='D',image_url='https://images.ctfassets.net/u0haasspfa6q/1uGsqwmSpOUN3ITzZ0akhp/7582e3072d2da76fdc3689527f808268/girl-in-the-sunset-on-some-rocks-on-the-water'
-    ,price=3,category='Food',quantity=2)
-    fourth = Product(user_id=3,name='Bad',description='B',image_url='https://images.ctfassets.net/u0haasspfa6q/1uGsqwmSpOUN3ITzZ0akhp/7582e3072d2da76fdc3689527f808268/girl-in-the-sunset-on-some-rocks-on-the-water'
-    ,price=4,category='Food',quantity=2)
+    first = Product(user_id=1,name='Biggest and best laptop ever',description='PLease buy one of these you will love them i promise',image_url='https://m.media-amazon.com/images/I/91shKLxoedL._AC_UY327_FMwebp_QL65_.jpg'
+    ,price=300,category='electronics',quantity=4)
+    second = Product(user_id=2,name='Very good desktop windows thingy everywhere haha',description='PLease buy one of these you will love them i promise xlarge computer',image_url='https://m.media-amazon.com/images/I/718sn7oOcfL._AC_UY327_FMwebp_QL65_.jpg'
+    ,price=500,category='electronics',quantity=4)
+    third= Product(user_id=3,name='Very good desktop windows thingy everywhere haha',description='I dont even know what this is but it looks cool',image_url='https://m.media-amazon.com/images/I/6109bE1DC0S._AC_UY327_FMwebp_QL65_.jpg'
+    ,price=60,category='electronics',quantity=10)
+    fourth = Product(user_id=3,name='Very good desktop windows thingy everywhere haha',description='Wow this one is so expensive',image_url='https://m.media-amazon.com/images/I/71A2llPqz7L._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=5000,category='electronics',quantity=3)
+
+
     fifth= Product(user_id=1,name='Terrible',description='C',image_url='https://images.ctfassets.net/u0haasspfa6q/1uGsqwmSpOUN3ITzZ0akhp/7582e3072d2da76fdc3689527f808268/girl-in-the-sunset-on-some-rocks-on-the-water'
     ,price=5,category='Food',quantity=2)
 
 
+    db.session.add(first)
+    db.session.add(second)
+    db.session.add(third)
+    db.session.add(fourth)
+    db.session.add(fifth)
+    db.session.add(first)
+    db.session.add(second)
+    db.session.add(third)
+    db.session.add(fourth)
+    db.session.add(fifth)
+    db.session.add(first)
+    db.session.add(second)
+    db.session.add(third)
+    db.session.add(fourth)
+    db.session.add(fifth)
     db.session.add(first)
     db.session.add(second)
     db.session.add(third)
--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -19,28 +19,22 @@ const Home = () => {
 
 
 
-    const productMap = products.map(el => {
+    // const productMap = products.map(el => {
 
-        return (
-            <div key={el?.id}>
-                <p >{el.name}</p>
-                <button onClick={(e) => deleteProductFunc(e, el?.id)}>Delete</button>
-                <NavLink to={`/product/update/${el?.id}`}><button  >Update</button></NavLink>
-            </div>
-        )
+    //     return (
+    //         <div key={el?.id}>
+    //             <p >{el.name}</p>
+    //             <button onClick={(e) => deleteProductFunc(e, el?.id)}>Delete</button>
+    //             <NavLink to={`/product/update/${el?.id}`}><button  >Update</button></NavLink>
+    //         </div>
+    //     )
 
-    })
+    // })
 
 
     const backgroundImgDivBigHome = (
         <div className='backgroundimgthingy'>
-            <div className='daddy-of-categories'>
 
-                <div className='categories-home-page'></div>
-                <div className='categories-home-page'></div>
-                <div className='categories-home-page'></div>
-                <div className='categories-home-page'></div>
-            </div>
 
         </div>
     )
@@ -57,12 +51,59 @@ const Home = () => {
 
     return (
         <>
-        <div className='something1'>
+            {/* <div className='something1'> */}
             <div className="home-main-div">
                 {/* {productMap} */}
                 {backgroundImgDivBigHome}
+                <div className='daddy-of-categories'>
+
+                    <div className='categories-home-page'>
+                        <NavLink exact to={'/allproducts/1'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>Electronics go brrrr!</p></NavLink>
+                        <img className='livinginthe' src='https://m.media-amazon.com/images/I/91shKLxoedL._AC_UY327_FMwebp_QL65_.jpg'></img>
+                        <img className='livinginthe' src='https://m.media-amazon.com/images/I/718sn7oOcfL._AC_UY327_FMwebp_QL65_.jpg'></img>
+                        <img  className='livinginthe' src='https://m.media-amazon.com/images/I/6109bE1DC0S._AC_UY327_FMwebp_QL65_.jpg'></img>
+                        <img  className='livinginthe' src='https://m.media-amazon.com/images/I/71A2llPqz7L._AC_UL480_FMwebp_QL65_.jpg'></img>
+
+                    </div>
+                    <div className='categories-home-page'>
+                        <NavLink exact to={'/allproducts/2'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>New clothing deals!</p></NavLink>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/315aUsDYSbL._SY500__AC_SY200_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/818Tnima1yL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/71G4kkPdU-L._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/515d7N7uQWS._AC_UL480_FMwebp_QL65_.jpg'></img>
+
+                    </div>
+                    <div className='categories-home-page'>
+                        <NavLink exact to={'/allproducts/3'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>Recommended furnitures!</p></NavLink>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/71-PSfCxooL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/61ZG-mJ6juL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/413vx3RglZL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/81zImSZ-ACL._AC_UL480_FMwebp_QL65_.jpg'></img>
+
+                    </div>
+                    <div className='categories-home-page'>
+                        <NavLink exact to={'/allproducts/4'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>Get your food here!</p></NavLink>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/81X1OIJiEFL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/A1dBzjL58SL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/81gtsep2+BL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/51PEKtJMQfL._AC_UL480_FMwebp_QL65_.jpg'></img>
+
+                    </div>
+                    <div className='somethingdiff'>
+
+                    </div>
+                    <div className='somethingdiff2'>
+                        <NavLink exact to={'/allproducts/5'}><p  style={{cursor:"pointer", fontSize: "21px", fontWeight: "bold" }}>Toys & Hobbies</p></NavLink>
+                        <img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/91yg3JBdlgL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/71JWVVP3nWL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/71wy2BJWFTL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <img className='womanclothing alsohehe' style={{marginRight:"0"}} src='https://m.media-amazon.com/images/I/81-jHs0yp8L._AC_UL480_FMwebp_QL65_.jpg'></img>
+
+                    </div>
+
+                </div>
             </div>
-            <div className='something'>
+            {/* <div className='something'>
                 <div className='alt-bolum-home-page-thingies'>
                     <div className='alt-left-part-categories'>
 
@@ -71,8 +112,8 @@ const Home = () => {
 
                     </div>
                 </div>
-            </div>
-        </div>
+            </div> */}
+            {/* </div> */}
         </>
 
     )
