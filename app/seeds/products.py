d6ea9626e50d6a3ecb68b54b9c4d561c647d946f
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-04-16 01:23:42 -0500

    Asdasdaasd

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -12,8 +12,41 @@ def seed_products():
     ,price=5000,category='electronics',quantity=3)
 
 
-    fifth= Product(user_id=1,name='Terrible',description='C',image_url='https://images.ctfassets.net/u0haasspfa6q/1uGsqwmSpOUN3ITzZ0akhp/7582e3072d2da76fdc3689527f808268/girl-in-the-sunset-on-some-rocks-on-the-water'
-    ,price=5,category='Food',quantity=2)
+    fifth= Product(user_id=1,name='Best woman clothe',description='Wow looks amazing',image_url='https://m.media-amazon.com/images/I/315aUsDYSbL._SY500__AC_SY200_.jpg'
+    ,price=6,category='fashion',quantity=5)
+    fifth2= Product(user_id=1,name='Nice man clothe',description='It will keep you wamr during winter',image_url='https://m.media-amazon.com/images/I/818Tnima1yL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=100,category='fashion',quantity=4)
+    fifth3= Product(user_id=2,name='Everyday tshirt',description='Something to be proud with',image_url='https://m.media-amazon.com/images/I/71G4kkPdU-L._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=50,category='fashion',quantity=3)
+    fifth4= Product(user_id=3,name='Very nice',description='Casdadsdasaa',image_url='https://m.media-amazon.com/images/I/515d7N7uQWS._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=20,category='fashion',quantity=2)
+
+    fifth5= Product(user_id=2,name='Furniture incredibleeeee',description='Furniture incredibleeeee really beyaasdasd',image_url='https://m.media-amazon.com/images/I/71-PSfCxooL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=30,category='furniture',quantity=1)
+    fifth6= Product(user_id=2,name='Furniture incredibleeeee',description='Furniture incredibleeeee',image_url='https://m.media-amazon.com/images/I/61ZG-mJ6juL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=50,category='furniture',quantity=10)
+    fifth7= Product(user_id=1,name='Furniture incredibleeeee',description='Furniture incredibleeeee',image_url='https://m.media-amazon.com/images/I/413vx3RglZL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=26,category='furniture',quantity=3)
+    fifth8= Product(user_id=3,name='Furniture incredibleeeee',description='Furniture incredibleeeee',image_url='https://m.media-amazon.com/images/I/81zImSZ-ACL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=15,category='furniture',quantity=6)
+
+    fifth9= Product(user_id=3,name='iM ALREADY HUNGARY',description='Are you hungry after seeing this you should be, its so good',image_url='https://m.media-amazon.com/images/I/81X1OIJiEFL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=76,category='food',quantity=2)
+    fifth10= Product(user_id=1,name='iM ALREADY HUNGARY',description='Are you hungry after seeing this you should be, its so goodAre you hungry after seeing this you should be, its so good',image_url='https://m.media-amazon.com/images/I/A1dBzjL58SL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=45,category='food',quantity=1)
+    fifth11= Product(user_id=2,name='iM ALREADY HUNGARY',description='Are you hungry after seeing this you should be, its so good',image_url='https://m.media-amazon.com/images/I/81gtsep2+BL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=34,category='food',quantity=7)
+    fifth12= Product(user_id=2,name='iM ALREADY HUNGARY',description='Are you hungry after seeing this you should be, its so good',image_url='https://m.media-amazon.com/images/I/51PEKtJMQfL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=500,category='food',quantity=8)
+
+    fifth13= Product(user_id=1,name='Something idk',description='I really dont know man',image_url='https://m.media-amazon.com/images/I/91yg3JBdlgL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=43,category='toys',quantity=9)
+    fifth14= Product(user_id=1,name='League of legends',description='Maaan I love this game',image_url='https://m.media-amazon.com/images/I/71JWVVP3nWL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=23,category='toys',quantity=4)
+    fifth15= Product(user_id=3,name='Car',description='Casdasdadasdadada',image_url='https://m.media-amazon.com/images/I/71wy2BJWFTL._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=5,category='toys',quantity=2)
+    fifth16= Product(user_id=3,name='toy ship',description='hehehehehe nbceee',image_url='https://m.media-amazon.com/images/I/81-jHs0yp8L._AC_UL480_FMwebp_QL65_.jpg'
+    ,price=1,category='toys',quantity=3)
 
 
     db.session.add(first)
@@ -21,21 +54,22 @@ def seed_products():
     db.session.add(third)
     db.session.add(fourth)
     db.session.add(fifth)
-    db.session.add(first)
-    db.session.add(second)
-    db.session.add(third)
-    db.session.add(fourth)
-    db.session.add(fifth)
-    db.session.add(first)
-    db.session.add(second)
-    db.session.add(third)
-    db.session.add(fourth)
-    db.session.add(fifth)
-    db.session.add(first)
-    db.session.add(second)
-    db.session.add(third)
-    db.session.add(fourth)
-    db.session.add(fifth)
+    db.session.add(fifth16)
+    db.session.add(fifth2)
+    db.session.add(fifth3)
+    db.session.add(fifth4)
+    db.session.add(fifth5)
+    db.session.add(fifth6)
+    db.session.add(fifth7)
+    db.session.add(fifth8)
+    db.session.add(fifth9)
+    db.session.add(fifth10)
+    db.session.add(fifth11)
+    db.session.add(fifth12)
+    db.session.add(fifth13)
+    db.session.add(fifth14)
+    db.session.add(fifth15)
+
 
     db.session.commit()
 
--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -59,34 +59,34 @@ const Home = () => {
 
                     <div className='categories-home-page'>
                         <NavLink exact to={'/allproducts/1'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>Electronics go brrrr!</p></NavLink>
-                        <img className='livinginthe' src='https://m.media-amazon.com/images/I/91shKLxoedL._AC_UY327_FMwebp_QL65_.jpg'></img>
-                        <img className='livinginthe' src='https://m.media-amazon.com/images/I/718sn7oOcfL._AC_UY327_FMwebp_QL65_.jpg'></img>
-                        <img  className='livinginthe' src='https://m.media-amazon.com/images/I/6109bE1DC0S._AC_UY327_FMwebp_QL65_.jpg'></img>
-                        <img  className='livinginthe' src='https://m.media-amazon.com/images/I/71A2llPqz7L._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <NavLink exact to={'/productpage/1'}><img className='livinginthe' src='https://m.media-amazon.com/images/I/91shKLxoedL._AC_UY327_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/2'}><img className='livinginthe' src='https://m.media-amazon.com/images/I/718sn7oOcfL._AC_UY327_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/3'}><img  className='livinginthe' src='https://m.media-amazon.com/images/I/6109bE1DC0S._AC_UY327_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/4'}><img  className='livinginthe' src='https://m.media-amazon.com/images/I/71A2llPqz7L._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
 
                     </div>
                     <div className='categories-home-page'>
                         <NavLink exact to={'/allproducts/2'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>New clothing deals!</p></NavLink>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/315aUsDYSbL._SY500__AC_SY200_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/818Tnima1yL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/71G4kkPdU-L._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/515d7N7uQWS._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <NavLink exact to={'/productpage/5'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/315aUsDYSbL._SY500__AC_SY200_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/7'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/818Tnima1yL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/8'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/71G4kkPdU-L._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/9'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/515d7N7uQWS._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
 
                     </div>
                     <div className='categories-home-page'>
                         <NavLink exact to={'/allproducts/3'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>Recommended furnitures!</p></NavLink>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/71-PSfCxooL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/61ZG-mJ6juL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/413vx3RglZL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/81zImSZ-ACL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <NavLink exact to={'/productpage/10'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/71-PSfCxooL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/11'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/61ZG-mJ6juL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/12'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/413vx3RglZL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/13'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/81zImSZ-ACL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
 
                     </div>
                     <div className='categories-home-page'>
                         <NavLink exact to={'/allproducts/4'}><p style={{ fontSize: "21px", fontWeight: "bold" }}>Get your food here!</p></NavLink>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/81X1OIJiEFL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/A1dBzjL58SL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/81gtsep2+BL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing' src='https://m.media-amazon.com/images/I/51PEKtJMQfL._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <NavLink exact to={'/productpage/14'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/81X1OIJiEFL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/15'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/A1dBzjL58SL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/16'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/81gtsep2+BL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/17'}><img className='womanclothing' src='https://m.media-amazon.com/images/I/51PEKtJMQfL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
 
                     </div>
                     <div className='somethingdiff'>
@@ -94,10 +94,10 @@ const Home = () => {
                     </div>
                     <div className='somethingdiff2'>
                         <NavLink exact to={'/allproducts/5'}><p  style={{cursor:"pointer", fontSize: "21px", fontWeight: "bold" }}>Toys & Hobbies</p></NavLink>
-                        <img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/91yg3JBdlgL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/71JWVVP3nWL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/71wy2BJWFTL._AC_UL480_FMwebp_QL65_.jpg'></img>
-                        <img className='womanclothing alsohehe' style={{marginRight:"0"}} src='https://m.media-amazon.com/images/I/81-jHs0yp8L._AC_UL480_FMwebp_QL65_.jpg'></img>
+                        <NavLink exact to={'/productpage/18'}><img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/91yg3JBdlgL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/19'}><img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/71JWVVP3nWL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/20'}><img className='womanclothing alsohehe' src='https://m.media-amazon.com/images/I/71wy2BJWFTL._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
+                        <NavLink exact to={'/productpage/6'}><img className='womanclothing alsohehe' style={{marginRight:"0"}} src='https://m.media-amazon.com/images/I/81-jHs0yp8L._AC_UL480_FMwebp_QL65_.jpg'></img></NavLink>
 
                     </div>
 
