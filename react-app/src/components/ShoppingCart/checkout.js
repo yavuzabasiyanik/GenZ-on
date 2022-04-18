b39b052c925c8618f79d2384f3afffe1b37157a1
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-04-18 00:01:27 -0500

    Asdasd

--- react-app/src/components/ShoppingCart/checkout.js
+++ react-app/src/components/ShoppingCart/checkout.js
@@ -4,7 +4,16 @@ const Checkout = () => {
 
     return (
         <div className="checkoutmaindiv">
-            
+            <div className='yesilli'>
+
+                <div className='bakma'>
+                    <i className='fas fa-check'></i>
+                    <p className='thankyou'> Thank you, your order has been placed.</p>
+                </div>
+
+                <p>Thank you for using GenZon.</p>
+            </div>
+
         </div>
     )
 }
