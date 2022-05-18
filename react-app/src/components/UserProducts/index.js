c1b126fa643905881bf240cdc76bdd4887763ae9
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-05-17 19:33:58 -0500

    kindadone heh

--- react-app/src/components/UserProducts/index.js
+++ react-app/src/components/UserProducts/index.js
@@ -2,14 +2,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import './UserProducts.css';
 import { deleteProduct } from '../../store/product';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import UpdateForm from '../ProductForm/UpdateForm';
 import Footer from '../Footer';
-// import { shoppingcartDeleteone } from '../../store/shoppingcart';
+
 const UserProducts = () => {
 
     const user = useSelector(state => state.session.user);
     const [updateProduct, setUpdateProduct] = useState([false, 0]);
+    const [deleteProductModal, setDeleteProductModal] = useState([false, -1]);
+
     const dispatch = useDispatch();
 
     const allProducts = useSelector(state => Object.values(state.products))
@@ -27,17 +29,30 @@ const UserProducts = () => {
 
         dispatch(deleteProduct(id));
 
-        // if( +shoppingcart?.product_id === +id && +shoppingcart?.user_id === +user?.id){
-        //     dispatch(shoppingcartDeleteone(Number(shoppingcart?.id)));
-        // }
-
-
+        setDeleteProductModal([false,-1])
     }
 
     useEffect(() => {
 
         window.scrollTo(0, 0)
     }, [])
+
+    let menuRef = useRef()
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (!menuRef?.current?.contains(event.target)) {
+
+                setDeleteProductModal([false, -1]);
+            }
+        }
+        document.addEventListener('mousedown', handler);
+
+        return () => {
+            document.removeEventListener('mousedown', handler)
+        }
+    });
+
 
     const allProductsUserDiv = userProducts.map(ele => {
         return (<div className='productsinuserpage' key={ele?.id}>
@@ -46,7 +61,7 @@ const UserProducts = () => {
 
                 <div className='nameusernamepricedescription'>
 
-                    <NavLink exact to={`/productpage/${ele?.id}`}><p style={{ color: "#007185", cursor: "pointer", fontSize: "14px", fontWeight: "700", display: "inline" }}>{ele?.name}</p></NavLink>
+                    <NavLink exact to={`/productpage/${ele?.id}`}><p className='turuncu' style={{ color: "#007185", cursor: "pointer", fontSize: "14px", fontWeight: "700", display: "inline" }}>{ele?.name}</p></NavLink>
                     <p style={{ lineHeight: "20px" }}><span style={{ fontSize: "10.5px" }}>by</span> <span style={{ fontWeight: "700" }}>{ele?.user.username}</span></p>
                     <p style={{ marginTop: "20px", fontSize: "14px", width: "500px", wordBreak: "break-word" }}>{ele?.description}</p>
                 </div>
@@ -58,14 +73,13 @@ const UserProducts = () => {
                     <p>Category: {ele?.category[0].toUpperCase() + ele?.category.slice(1)}</p>
                 </div>
                 <button className='yellowupdatethingy' onClick={(e) => setUpdateProduct([true, ele?.id])} >Update</button>
-                <button className='reddeleteproduct' onClick={(e) => deleteProductFunc(e, ele?.id)}>Delete</button>
+                <button className='reddeleteproduct' onClick={(e) => setDeleteProductModal([true,ele?.id])}>Delete</button>
             </div>
         </div>)
     })
 
 
-    // background-color: #ffd814;
-    // border-color: #FCD200;
+
 
     const legendaryLeagueEmptyImage = (
         <div className='legendmain-div'>
@@ -74,8 +88,8 @@ const UserProducts = () => {
                     <div className='h1veh6ikilisi'>
 
                         <h1 style={{ marginLeft: "10px", fontSize: "28px" }}>Wanna sell your item on GenZon? Click
-                         <NavLink  exact to={`/product/sell`}><span style={{margin: "0px 10px", fontSize: "28px", color: "#F08804", textDecoration:"underline" }}>here</span></NavLink>
-                        to learn more.</h1>
+                            <NavLink exact to={`/product/sell`}><span style={{ margin: "0px 10px", fontSize: "28px", color: "#F08804", textDecoration: "underline" }}>here</span></NavLink>
+                            to learn more.</h1>
                     </div>
 
                 </div>
@@ -94,7 +108,7 @@ const UserProducts = () => {
                     <div className='productsGrid'>
                         {allProductsUserDiv}
                     </div>
-                    <Footer something={true}/>
+                    <Footer something={true} />
                 </div>)
                 : (
                     legendaryLeagueEmptyImage
@@ -103,6 +117,25 @@ const UserProducts = () => {
             }
             {updateProduct[0] &&
                 <UpdateForm id={updateProduct[1]} update={updateProduct} setUpdate={setUpdateProduct} />
+            }
+            {deleteProductModal[0] &&
+                <div className='background-modal'>
+                    <div ref={menuRef} className="modal-container-edit-delete-post-modal">
+                        <div className="areYouSureYouwannaDelete" >
+                            <div className='pareyousure'>
+                                <p className='deletePostA'>Delete product?</p>
+                                <p className='areyousuredeletepost'>Are you sure you want to remove this product from GenZon?</p>
+                            </div>
+                            <div onClick={(e) => deleteProductFunc(e, deleteProductModal[1])} className="yes">
+                                {/* edit-delete-post-modal-divs navlink-delete */}
+                                <button  className="navlink-delete">Yes</button>
+                            </div>
+                            <div className="edit-delete-post-modal-divs" onClick={() => setDeleteProductModal([false,-1])}>
+                                Cancel
+                            </div>
+                        </div>
+                    </div>
+                </div>
             }
         </>
 
